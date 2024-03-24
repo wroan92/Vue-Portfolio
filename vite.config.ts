538687ce93065850d9ab2df8d0c58a346d5499da
Anders Wroldsen <Anders-wroldsen@live.com>
2024-03-24 16:09:50 +0100

    Add base URL and proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'http://anderswroldsen.com/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
