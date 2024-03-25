25653313da53bd9be80afaa09583c3bcbb47efd3
Anders Wroldsen <Anders-wroldsen@live.com>
2024-03-25 16:39:42 +0100

    Remove axios dependency and update package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-/*   base: 'http://anderswroldsen.com/', */
+ /*  base: 'http://anderswroldsen.com/', */
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
